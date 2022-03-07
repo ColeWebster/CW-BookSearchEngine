e1412de6c4d5a78c8c6b02fc6260a7892643e370
Cole Webster <Cole.Webster@gmail.com>
2022-03-07 15:24:48 -0500

    Working on typedefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,18 @@
 const { Book, User } = require('../models');
 
 const resolvers = {
+    Query: {
+        book: async () => {
+            return Book.find({});
+        }
+        user: async () => {
+            return User.find({});
+        }
+    },
 
+    Mutation: {
+        
+    }
 }
 
 module.exports = resolvers; 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,32 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type Book {
+        authors: String!
+        description: String!
+        _id: ID!
+        image: String!
+        link: String!
+        title: String!
+    }
+
+    type User {
+        username: String!
+        email: String!
+        password: String!
+        savedBook: [Book]
+    }
+
+    type Query {
+        book: [Book]
+    }
+
+    typeMutation {
+        login
+        addUser
+        saveBook
+        removeBook
+    }
 `;
 
 module.exports = typeDefs
